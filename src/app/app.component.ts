7dbf4b075e2d2819449fef3413c393b8104dd1a2
tahsinturag <tahsinturag10@gmail.com>
2024-11-20 13:07:47 +0600

    some functionalities added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {EmployeeModel} from './model/Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_18_crud';
+  employeeForm: FormGroup = new FormGroup({});
+  employeeObj: EmployeeModel = new EmployeeModel();
+
+  constructor() {
+    this.createForm();
+}
+  createForm() {
+    this.employeeForm = new FormGroup({
+      empid: new FormControl(this.employeeObj.empId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+      state: new FormControl(this.employeeObj.state),
+      emailId: new FormControl(this.employeeObj.emailId),
+      contactNo: new FormControl(this.employeeObj.contactNo),
+      address: new FormControl(this.employeeObj.address),
+      pinCode: new FormControl(this.employeeObj.pinCode)
+    });
+  }
 }
